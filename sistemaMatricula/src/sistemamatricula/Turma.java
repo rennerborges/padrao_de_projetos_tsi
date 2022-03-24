26cd7af5a69d8a3d8ad288ee8743e356ae757a6e
Renner Borges <rennerferreira23@gmail.com>
2022-03-23 22:33:46 -0300

    feat: iniciando classe de Turma

---
+++ sistemaMatricula/src/sistemamatricula/Turma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemamatricula;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author renner
+ */
+public class Turma {
+    private ArrayList<Aluno> alunos = new ArrayList<>();
+    private ArrayList<Professor> professores = new ArrayList<>();
+    private String nome;
+    private String periodo;
+    private Date dataInicio;
+    private Date dataFim;
+
+}
