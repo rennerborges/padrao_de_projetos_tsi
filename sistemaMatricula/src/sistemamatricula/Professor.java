f8a928fc9d1a75bf1ca98fc26b2d43cdea873b9e
Renner Borges <rennerferreira23@gmail.com>
2022-03-23 22:33:35 -0300

    feat: iniciando classe de Professor

---
+++ sistemaMatricula/src/sistemamatricula/Professor.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemamatricula;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author renner
+ */
+public class Professor extends Pessoa {
+    private String especializacao;
+
+    public Professor(String especializacao, String nome, String sobrenome, Date dataNacimento, String cpf) {
+        super(nome, sobrenome, dataNacimento, cpf);
+        this.especializacao = especializacao;
+    }
+
+    public String getEspecializacao() {
+        return especializacao;
+    }
+
+    public void setEspecializacao(String especializacao) {
+        this.especializacao = especializacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + Objects.hashCode(this.especializacao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Professor other = (Professor) obj;
+        return Objects.equals(this.especializacao, other.especializacao);
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + "especializacao=" + especializacao + '}';
+    }
+    
+}
