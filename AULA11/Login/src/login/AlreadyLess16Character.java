343da2c886513c24b1fae411e8d7b6fcecda2783
Renner Borges <rennerferreira23@gmail.com>
2022-05-22 12:21:46 -0300

    feat: terminando loǵica do exercicio da AULA11

---
+++ AULA11/Login/src/login/AlreadyLess16Character.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+/**
+ *
+ * @author renner
+ */
+
+public class AlreadyLess16Character extends PasswordChain {
+
+    public AlreadyLess16Character(IDStagePassword id, String description) {
+        super(id, description);
+    }
+
+    @Override
+    public boolean isValidPassword(String password) {
+        return password.length() <= 16;
+    }
+}
--- AULA11/Login/src/login/Main.java
+++ AULA11/Login/src/login/Main.java
@@ -4,15 +4,20 @@
  */
 package login;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author renner
  */
 public class Main {
+    public static final String ANSI_RED_BACKGROUND = "\u001B[31m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[32m";
 
     /**
      * @param args the command line arguments
-     */
+     */   
+    
     public static void main(String[] args) {
         // TODO code application logic here
 
@@ -22,11 +27,15 @@ public class Main {
         passwordChain.setNext(new AlreadySpecialCharacter(IDStagePassword.STAGE4, "Se a senha possui pelo menos um dos caractere especial (Ex.: @, #, $, %, &, *)"));
         passwordChain.setNext(new AlreadyNumberCharacter(IDStagePassword.STAGE5, "Se a senha possui um número"));
         passwordChain.setNext(new AlreadySequenceOfNumbers(IDStagePassword.STAGE6, "Se a senha não possui 3 números consecutivos na sequencia (Ex: Ab123)"));
+        passwordChain.setNext(new Already8Character(IDStagePassword.STAGE7, "Se a senha tem pelo menos 8 caracteres"));
+        passwordChain.setNext(new AlreadyLess16Character(IDStagePassword.STAGE8, "Se a senha não tem mais de 16 caracteres"));
 
         try {
-            passwordChain.verifyPassword("12m2R*");
+            String password = JOptionPane.showInputDialog("Informe a senha:");
+            passwordChain.verifyPassword(password);
+            System.out.println(ANSI_GREEN_BACKGROUND+"A senha é valida");
         } catch (Exception e) {
-            System.out.println("Erro: " + e.getMessage());
+            System.out.println(ANSI_RED_BACKGROUND+"Erro: " + e.getMessage());
         }
     }
 
